a07e4e80d58a04e8700cf50fb5ce86db9171347f
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-07-23 16:36:03 -0400

    character model

--- src/character/domain/model.py
+++ src/character/domain/model.py
@@ -1,2 +1,12 @@
 class Character:
-    pass
+    def __init__(self, character_id, name, description, year_of_birth, month_of_birth, day_of_birth, status, gender,
+                 life_status):
+        self.id = character_id
+        self.name = name
+        self.description = description
+        self.year_of_birth = year_of_birth
+        self.month_of_birth = month_of_birth
+        self.day_of_birth = day_of_birth
+        self.status = status
+        self.gender = gender
+        self.life_status = life_status
