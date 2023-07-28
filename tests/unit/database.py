db70178a4913c77c4c7f04007a20f150c17678fb
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-07-27 20:41:40 -0400

    pruebas en bd

--- tests/unit/database.py
+++ tests/unit/database.py
@@ -7,7 +7,10 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 engine = db.create_engine(
-        f'mysql+mysqldb://{os.environ.get("MYSQL_TEST_USER")}:{os.environ.get("MYSQL_TEST_PASSWORD")}@{os.environ.get("MYSQL_TEST_HOST")}/{os.environ.get("MYSQL_TEST_DATABASE")}')
+    f'mysql+mysqldb://{os.environ.get("MYSQL_TEST_USER")}:{os.environ.get("MYSQL_TEST_PASSWORD")}@' +
+    f'{os.environ.get("MYSQL_TEST_HOST")}/{os.environ.get("MYSQL_TEST_DATABASE")}')
 
 Session = sessionmaker(engine)
 session = Session()
+
+session.connection()
