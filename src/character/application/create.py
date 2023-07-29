4025d4c8acabf6787b7a2987644182ed0789f33f
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-07-29 01:07:28 -0400

    create testing

--- src/character/application/create.py
+++ src/character/application/create.py
@@ -19,12 +19,12 @@ class Create(Action):
             gender = str(gender)
 
         character = Character(name=name, description=description, status=status, gender=gender, life_status=life_status)
-        character.sanitize_for_mysql()
         if character.validate_create() is False:
             errors = character.get_errors()
             self.set_errors(errors)
             return False
 
+        character.sanitize_for_mysql()
         query = insert(CharacterORM).values(name=character.name, description=character.description,
                                             status=character.status, gender=character.gender,
                                             life_status=character.life_status)
