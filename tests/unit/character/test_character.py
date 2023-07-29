264f98ff0d7e92413f26af3e459d5b50153aa20d
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-07-29 01:13:11 -0400

    unificacion de test

--- tests/unit/character/test_character.py
+++ tests/unit/character/test_character.py
@@ -1,14 +1,45 @@
 import pytest
-from character.application import List
-from character.domain import CharacterORM
-from unittest.mock import MagicMock
 
+from character.application import Create, List
+from character.domain import Character, CharacterORM
 from unit.character import test_params
 from unit.database import session
 
 
-class TestListAction:
-    # Test cases for List action class
+class TestCharacter:
+    @pytest.mark.parametrize("character_id, name, description, status, gender, life_status, expectation", test_params)
+    def test_validate_create(self, character_id, name, description, status, gender, life_status, expectation):
+        if name is not None:
+            name = str(name)
+
+        if description is not None:
+            description = str(description)
+
+        if status is not None:
+            status = str(status)
+
+        if gender is not None:
+            gender = str(gender)
+
+        character = Character(name=name, description=description, status=status, gender=gender, life_status=life_status)
+        result = character.validate_create()
+
+        print(character.get_errors())
+        # Validating the result
+        assert isinstance(result, bool) and result == expectation
+
+    @pytest.mark.parametrize("character_id, name, description, status, gender, life_status, expectation", test_params)
+    def test_create_character(self, character_id, name, description, status, gender, life_status, expectation):
+        # Mocking the session object
+        create_action = Create(session)
+
+        # Executing the action
+        result = create_action.execute(name, description, status, gender, life_status)
+
+        # Validating the result
+        print(create_action.get_errors())
+        assert isinstance(result, bool) and result == expectation
+
     @pytest.mark.parametrize("character_id, name, description, status, gender, life_status, expectation", test_params)
     def test_list_characters(self, character_id, name, description, status, gender, life_status, expectation):
         character_orm = CharacterORM(name=name, description=description, status=status, gender=gender,
