07bf4330db2c7c05b083f94b83f0ac026cfb0777
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-08-23 01:33:43 -0400

    delete milestone

--- src/milestone/application/delete.py
+++ src/milestone/application/delete.py
@@ -1,5 +1,16 @@
+from sqlalchemy import delete
+
 from common.domain import Action
+from milestone.domain import MilestoneORM
 
 
 class Delete(Action):
-    pass
+    def execute(self, milestone_id):
+        query = delete(MilestoneORM).where(MilestoneORM.id == milestone_id)
+        try:
+            result = self.session.execute(query)
+            self.session.commit()
+            return result.rowcount
+        except Exception as err:
+            self.add_error(str(err))
+            return False
