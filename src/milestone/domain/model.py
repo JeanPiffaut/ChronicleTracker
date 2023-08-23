0c609aabc89f13c6e9db785e37305e22332dcf12
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-08-22 23:03:17 -0400

    definiendo modulos

--- src/milestone/domain/model.py
+++ src/milestone/domain/model.py
@@ -1,2 +1,31 @@
-class Milestone:
-    pass
+from flask_restful.fields import DateTime
+
+from common.domain import ModuleModel
+from milestone.domain import MilestoneORM
+
+
+class Milestone(ModuleModel):
+    def __init__(self, milestone_id: int = None, description: str = None, date: DateTime = None):
+        super().__init__()
+        self.id = milestone_id
+        self.description = description
+        self.date = date
+
+    def set_by_module_orm(self, obj: MilestoneORM):
+        if obj.id is not None:
+            self.id = obj.id
+
+        if obj.description is not None:
+            self.description = obj.description
+
+        if obj.date is not None:
+            self.date = obj.date
+
+    def validate_create(self):
+        has_error = not super().validate()
+        if self.description is None or self.description.strip() == "":
+            self.add_error(f'The \'description\' field cannot be None or Empty', ValueError)
+            has_error = True
+
+        return not has_error
+
