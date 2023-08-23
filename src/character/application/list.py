5fe091f4d2e292b04506bc1704f2f3a617ab2a38
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2023-08-23 01:19:06 -0400

    listar milestone

--- src/character/application/list.py
+++ src/character/application/list.py
@@ -37,10 +37,10 @@ class List(Action):
 
         result = self.session.execute(query)
 
-        user_list = list()
-        for user_obj in result.scalars():
+        character_list = list()
+        for character_obj in result.scalars():
             char = Character()
-            char.set_by_module_orm(user_obj)
-            user_list.append(char.to_dict())
+            char.set_by_module_orm(character_obj)
+            character_list.append(char.to_dict())
 
-        return user_list
+        return character_list
--- src/milestone/application/list.py
+++ src/milestone/application/list.py
@@ -1,5 +1,7 @@
+from sqlalchemy import select
+
 from common.domain import Action
-from milestone.domain import Milestone
+from milestone.domain import Milestone, MilestoneORM
 
 
 class List(Action):
@@ -8,4 +10,28 @@ class List(Action):
         self.fill = Milestone()
 
     def execute(self):
-        pass
+        fill_params = self.fill
+        if fill_params.validate() is False:
+            errors = fill_params.get_errors()
+            self.set_errors(errors)
+            return False
+
+        query = select(MilestoneORM)
+        if fill_params.id is not None:
+            query = query.where(MilestoneORM.id == fill_params.id)
+
+        if fill_params.description is not None:
+            query = query.where(MilestoneORM.description == fill_params.description)
+
+        if fill_params.date is not None:
+            query = query.where(MilestoneORM.date == fill_params.date)
+
+        result = self.session.execute(query)
+
+        milestone_list = list()
+        for milestone_obj in result.scalars():
+            char = Milestone()
+            char.set_by_module_orm(milestone_obj)
+            milestone_list.append(char.to_dict())
+
+        return milestone_list
